6ab395e58dcc5c6eb191892b2ed565a1befa5562
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-09-25 14:47:04 +0100

    feat: add IRepositoryBase to Contracts

---
+++ ErrorSol/Migrations/20200925133410_InitialMigration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ErrorSol.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    CompanyId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 60, nullable: false),
+                    Address = table.Column<string>(maxLength: 60, nullable: false),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.CompanyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    Position = table.Column<string>(maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
