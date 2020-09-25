5846b7f495b3fbff4355be6aa4102adf3ba27433
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-09-25 14:21:14 +0100

    feat: add ApplicationDbContext

---
+++ ErrorSol/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ErrorSol.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorSol
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
