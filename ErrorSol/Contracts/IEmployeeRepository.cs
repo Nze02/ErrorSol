905b7e9a00ba07cc9b92b80af0bb3763ae836b7c
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-09-28 20:17:39 +0100

    feat: add IEmployeeRepository

---
+++ ErrorSol/Contracts/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using ErrorSol.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorSol.Contracts
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllEmployeesAsync(bool trackChanges);
+        Task<Employee> GetEmployeeAsync(Guid Id, bool trackchanges);
+        void CreateEmployee(Employee Employee);
+        void DeleteEmployee(Employee Employee);
+    }
+}
