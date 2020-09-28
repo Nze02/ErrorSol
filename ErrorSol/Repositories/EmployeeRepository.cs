2728710feaf78a09627ec51f6a1252e483fd13de
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-09-28 20:27:21 +0100

    feat: implement EmployeeRepository

---
+++ ErrorSol/Repositories/EmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using ErrorSol.Contracts;
+using ErrorSol.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorSol.Repositories
+{
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(ApplicationDbContext _db) : base(_db)
+        {
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployeesAsync(bool trackChanges) =>
+            await FindAll(trackChanges)
+            .OrderBy(e => e.Id)
+            .ToListAsync();
+
+        public async Task<Employee> GetEmployeeAsync(Guid Id, bool trackchanges) =>
+            await FindByCondition(e => e.Id.Equals(Id), trackchanges)
+            .SingleOrDefaultAsync();
+
+        public void CreateEmployee(Employee Employee) => Create(Employee);
+
+        public void DeleteEmployee(Employee Employee) => Delete(Employee);
+    }
+}
