4b1fc1cea860aa648803c5440e14a3cce6acdb1f
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-09-28 21:04:09 +0100

    feat: add Result

---
+++ ErrorSol/Entities/ReturnFormat/Result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorSol.Entities.ReturnFormat
+{
+    public class Result<T>
+    {
+        [Required(ErrorMessage = "Result message is required")]
+        public string Message { get; set; }
+        public IEnumerable<T> Data { get; set; }
+    }
+}
