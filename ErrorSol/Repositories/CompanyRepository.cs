aa4c7beb21cdc142cea8daa9e1b5133501369cfc
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-09-28 20:15:15 +0100

    feat: implement CompanyRepository

---
+++ ErrorSol/Repositories/CompanyRepository.cs
@@ -0,0 +1,30 @@
+﻿using ErrorSol.Contracts;
+using ErrorSol.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorSol.Repositories
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(ApplicationDbContext _db) : base(_db)
+        {
+        }
+
+        public async Task<IEnumerable<Company>> GetAllCompaniesAsync(bool trackChanges) =>
+            await FindAll(trackChanges)
+            .OrderBy(e => e.Id)
+            .ToListAsync();
+
+        public async Task<Company> GetCompanyAsync(Guid Id, bool trackchanges) =>
+            await FindByCondition(e => e.Id.Equals(Id), trackchanges)
+            .SingleOrDefaultAsync();
+
+        public void CreateCompany(Company company) => Create(company);
+
+        public void DeleteCompany(Company company) => Delete(company);
+    }
+}
