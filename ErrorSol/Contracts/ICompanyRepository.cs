27e98901f3e98fd1e521ea1d9abf7c4c27217c15
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-09-28 20:06:20 +0100

    feat: Add ICompanyRepository

---
+++ ErrorSol/Contracts/ICompanyRepository.cs
@@ -0,0 +1,16 @@
+﻿using ErrorSol.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorSol.Contracts
+{
+    public interface ICompanyRepository
+    {
+        Task<IEnumerable<Company>> GetAllCompaniesAsync(bool trackChanges);
+        Task<Company> GetCompanyAsync(Guid Id, bool trackchanges);
+        void CreateCompany(Company company);
+        void DeleteCompany(Company company);
+    }
+}
